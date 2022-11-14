ca874905e51eeb0e931135c36e9a52d238110843
Joanne Lin <joannelin@Joannes-Mac.local>
2022-11-14 01:01:58 -0600

    I modified the getadjacent method return type. Added the graph to be weighted graph

--- SymbolGraph.java
+++ SymbolGraph.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
@@ -8,12 +9,13 @@ import java.util.TreeMap;
  */
 public class SymbolGraph<E> 
 {
-	private Map<E, List<E>> edges;
+	private Map<E, Map<E, Integer>> edges;
 	private int numberOfEdges;
+	
 	
 	public SymbolGraph()
 	{
-		edges = new TreeMap<E, List<E>>();
+		edges = new TreeMap<E, Map<E, Integer>>();
 	}
 
 	
@@ -34,43 +36,48 @@ public class SymbolGraph<E>
 			return false;
 		}
 		
-		edges.put(value, new ArrayList<>());
+		edges.put(value, new TreeMap<E, Integer>());
 		return true;
 	}
 	
-	public boolean addEdge(E from, E to)
+	public boolean addEdge(E from, E to, int weight)
 	{
-		if (edges.get(from)!=null && edges.get(to)!=null) {
-			if (edges.get(from).contains(to)) {
-				System.out.println("what the");
+		if (edges.containsKey(from) && edges.containsKey(to)) {
+			if (edges.get(from).containsKey(to)) {
+				System.out.println("The edge between "+ from +" and "+to+" already existed.");
 				return false;
-			}
-			edges.get(from).add(to);
-			edges.get(to).add(from);
+			} 
+			edges.get(from).put(to, weight);
+			edges.get(to).put(from, weight);
 			
 			numberOfEdges++;
 			return true;
 		}
 		
-		
+		if (!edges.containsKey(from)) {
+			System.out.println("There is no vertex"+from);
+		}
+		if (!edges.containsKey(to)) {
+			System.out.println("There is no vertex "+to);
+		}
 		return false;
 		
 		
 	}
 
-	public List<E> getAdjacent(E to)
+	public Set<E> getAdjacent(E to)
 	{
-		if (edges.get(to)==null) {return null;}
-		return edges.get(to);
+		if (edges.get(to)==null || dges.get(to).isEmpty()) {return null;}
+		return edges.get(to).keySet();
 	}
 
 	public boolean removeEdge(E from, E to)
 	{
 		if (edges.get(from)==null) {return false;}
-		if (!edges.get(from).contains(to)) {return false;}
+		if (!edges.get(from).containsKey(to)) {return false;}
 		numberOfEdges--;
 		
-		edges.get(from).remove(to);
+		edges.get(from).remove(key);
 		edges.get(to).remove(from);
 		return true;
 	}	
