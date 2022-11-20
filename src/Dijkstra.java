a06b53c03a09d4f03e1500fe417aedba3d29cad4
Joanne Lin <joannelin@Joannes-Mac.local>
2022-11-20 15:11:42 -0600

    changing it to a project file

---
+++ src/Dijkstra.java
@@ -0,0 +1,96 @@
+package graphs;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+	
+    class temp{
+        int vertex;
+        int distance; // current distance of source to vertex
+        public temp(int vertex, int distance){
+            this.vertex = vertex;
+            this.distance =  distance;
+        }
+    }
+
+  public static void dijkstra(SymbolGraph<Integer> NewGraph, int source) {
+	  
+    int count = NewGraph.getNumberOfVerticies();
+    boolean[] visitedVertex = new boolean[count];
+    int[] distance = new int[count];
+    
+    for (int i = 0; i < count; i++) {
+        
+      visitedVertex[i] = false;
+      distance[i] = Integer.MAX_VALUE;
+    }
+    
+       // creating priority object to store nodes with their distance from source
+        PriorityQueue<temp> objectQ = new PriorityQueue<>((Comparator.comparingInt(o -> o.distance)));
+	  
+        // Distance of self loop is zero
+        distance[source] = 0;
+	  
+        //now adding source node to priority objectQ
+        objectQ.add(new temp(source,distance[source]));
+
+        while (objectQ.size()>0){
+
+            temp NewNode = objectQ.poll(); // to get out the minimum distance node from priority objectQ
+
+            if(visitedVertex[NewNode.vertex])continue; // if NewNode is visited, do not add it or skip it
+
+            visitedVertex[NewNode.vertex] = true; // if not visited mark as visited 
+		
+	    // creating new map that take edges from current nodes
+
+            Map<Integer, Integer> adjacentEdges = NewGraph.getAdjacent(NewNode.vertex); 
+	
+	    // now loop through the adjacent nodes 
+            for(int adjacentNode : adjacentEdges.keySet()){// adjacent nodes
+
+                if(visitedVertex[adjacentNode])continue; // if adjacent node visited, do not add or skip it
+		    
+		 // if new distance of adjacent new node is less than already noted distance than change id and add to objectQ
+                if(NewNode.distance + adjacentEdges.get(adjacentNode) < distance[adjacentNode]){ 
+                    // updat distance                                                                    
+                    distance[adjacentNode] = NewNode.distance + adjacentEdges.get(adjacentNode);
+			
+		  //  finally add the shortest distance to objectQ
+                    objectQ.add(new temp(adjacentNode,distance[adjacentNode])); 
+                }
+            }
+        }
+        System.out.println(Arrays.toString(distance));
+// a helpful website we used to create the dijkstra method https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-using-priority_queue-stl/
+}
+ public static void main(String[] args) {
+     
+        SymbolGraph<Integer> draw = new SymbolGraph<>();
+
+        int NewGraph[][] = new int[][] { { 0, 0, 1, 2, 0, 0, 0 }, { 0, 0, 2, 0, 0, 3, 0 },
+                { 1, 2, 0, 1, 3, 0, 0 },{ 2, 0, 1, 0, 0, 0, 1 },
+                { 0, 0, 3, 0, 0, 2, 0 }, { 0, 3, 0, 0, 2, 0, 1 },
+                { 0, 0, 0, 1, 0, 1, 0 } };
+                
+        // use loop to add vertex to graph class
+	 
+        for (int i=0;i < NewGraph.length;i++)draw.addVertex(i);
+        
+        // use loop to add edges to edges class
+	 
+        for (int i=0; i< NewGraph.length;i++){
+            for(int j = 0;j < NewGraph.length;j++){
+                if(NewGraph[i][j]!=0){
+			
+                    draw.addEdge(i,j,NewGraph[i][j]);
+                }
+            }
+        }
+        Dijkstra D= new Dijkstra();
+
+        D.dijkstra(NewGraph, 0); //print short distance of all other nodes from node 0
+    }
+}
---
+++ src/SymbolGraph.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * @author <Joanne Lin>
+ */
+public class SymbolGraph<E> 
+{
+	private Map<E, Map<E, Integer>> edges;
+	private int numberOfEdges;
+	
+	
+	public SymbolGraph()
+	{
+		edges = new TreeMap<E, Map<E, Integer>>();
+	}
+
+	
+	public int getNumberOfVerticies()
+	{
+		return edges.size();
+	}
+
+	
+	public int getNumberOfEdges()
+	{
+		return numberOfEdges;
+	}
+
+	public boolean addVertex(E value)
+	{
+		if (edges.containsKey(value)) {
+			return false;
+		}
+		
+		edges.put(value, new TreeMap<E, Integer>());
+		return true;
+	}
+	
+	public boolean addEdge(E from, E to, int weight)
+	{
+		if (edges.containsKey(from) && edges.containsKey(to)) {
+			if (edges.get(from).containsKey(to)) {
+				System.out.println("The edge between "+ from +" and "+to+" already existed.");
+				return false;
+			} 
+			edges.get(from).put(to, weight);
+			edges.get(to).put(from, weight);
+			
+			numberOfEdges++;
+			return true;
+		}
+		
+		if (!edges.containsKey(from)) {
+			System.out.println("There is no vertex"+from);
+		}
+		if (!edges.containsKey(to)) {
+			System.out.println("There is no vertex "+to);
+		}
+		return false;
+		
+		
+	}
+
+	public Map<E, Integer> getAdjacent(E to)
+	{
+		if (edges.get(to)==null || edges.get(to).isEmpty()) {return null;}
+		return edges.get(to);
+	}
+
+	public boolean removeEdge(E from, E to)
+	{
+		if (edges.get(from)==null) {return false;}
+		if (!edges.get(from).containsKey(to)) {return false;}
+		numberOfEdges--;
+		
+		edges.get(from).remove(to);
+		edges.get(to).remove(from);
+		return true;
+	}	
+}
