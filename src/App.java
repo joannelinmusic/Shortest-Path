d7ab7ea3011ca58ea30a0bf00b0b93ba7227c39c
Joanne Lin <joannelin@Joannes-Mac.local>
2022-11-20 18:54:02 -0600

    added vertex

--- src/App.java
+++ src/App.java
@@ -12,8 +12,24 @@ public class App {
         System.out.println("Hi " + userName + ", Let's create a graph!");
 
         //Direct user to create a graph
-        SymbolGraph<Integer> userDraw = new SymbolGraph<>();
-        System.out.println("Your currently have " + userDraw.getNumberOfVerticies() + "vertices");
+        SymbolGraph<String> userDraw = new SymbolGraph<>();
+        System.out.println("Add your first vertex: ");
+        String vertex = scan.nextLine();
+        userDraw.addVertex(vertex);
+
+        System.out.println("Would you like to add more vertex? (Enter only true or false)");
+        Boolean addBoolean = scan.nextBoolean();
+
+        while (addBoolean){
+            scan.nextLine();
+            System.out.println("Add your next vertex: ");
+            vertex = scan.nextLine();
+            userDraw.addVertex(vertex);
+            System.out.println("Would you like to add more vertex? (Enter only true or false)");
+            addBoolean = scan.nextBoolean();
+        }
+
+        System.out.println("Your currently have " + userDraw.getNumberOfVerticies() + " vertices");
 
 
 
