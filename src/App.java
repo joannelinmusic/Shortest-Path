1543955439e5f201362dd6b125b421463f8bda99
Joanne Lin <joannelin@Joannes-Mac.local>
2022-11-20 22:56:06 -0600

    modified

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+
 import java.util.Arrays;
+import java.util.Map;
 
 
 public class App {
@@ -13,19 +15,30 @@ public class App {
         scan = new Scanner(System.in);
         System.out.println("Enter Name: ");
         userName = scan.nextLine();   
-        System.out.println("Hi " + userName + ", Let's create a graph!");
+        System.out.println("Hi " + userName + ", Let's create an graph!");
 
         //Direct user to create a graph
         userDraw = new SymbolGraph<>();
 
         addHelper();
+
+        System.out.println("Now let's find the distances of vertex. Which vertex would you like to set as the source vertex?");
+        input = scan.nextLine();
+
+        Dijkstra d = new Dijkstra();
+        Map<String, Integer> distance = d.dijkstra(userDraw, input);
+        for (Map.Entry<String, Integer>  entry: distance.entrySet()) {
+            System.out.println("The shortest distance to "+entry.getKey()+" is " + entry.getValue());
+        }
+
     }
 
 
 
     private static void addHelper(){
         //add first vertex
-        System.out.println("Add your first vertex: \n Type 'Vertex: your vertex name'");
+        System.out.println("You will need at least two vertex and one edge to continue. All vertex must be an integer.");
+        System.out.println("Add your first vertex: \n      Example: 'Vertex: A'");
         String input = scan.nextLine();
         
         //add more vertex and edge
@@ -45,18 +58,14 @@ public class App {
             System.out.println("You currently have " + userDraw.getNumberOfVerticies() + " vertices and " + userDraw.getNumberOfEdges() + " edges.");
             
             if (userDraw.getNumberOfVerticies()>=2){
-                System.out.println("Add your next vertex: (See format below) \n Adding a Vertex: 'Vertex: your vertex name' \n Adding an edge: (all seperated by space) 'Edge: Vertex_1 Vertex_2 Weight(integer)' \n If you're done: Type 'done' ");
+                
+                System.out.println("Add your next vertex: (See format below) \n Adding a Vertex: 'Vertex: your vertex name' \n      Example: 'Vertex: A' \n Adding an edge: Type 'Edge: your VertexOneName your VertexTwoName Weight(any integer) '\n       Example: 'Edge: A B 2' \n If you're done: Type 'done' ");
             } else {
-                System.out.println("Add your next vertex: \n Type 'Vertex: your vertex name' ");
+                System.out.println("Add your next vertex: \n      Example: 'Vertex: A'");
             }
             
             input = scan.nextLine();
         }
-
-
-        
-
-        //scan.nextLine();
         
     }
 }
--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,96 +1,97 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 
 public class Dijkstra {
 
 	
-    static class Temp{
-        int vertex;
-        int distance; // current distance of source to vertex
-        public Temp(int vertex, int distance){
-            this.vertex = vertex;
-            this.distance =  distance;
-        }
-    }
 
-    public void dijkstra(SymbolGraph<Integer> NewGraph, int source) {
+    public Map<String, Integer> dijkstra(SymbolGraph<String> NewGraph, String source) {
         
-        int count = NewGraph.getNumberOfVerticies();
-        boolean[] visitedVertex = new boolean[count];
-        int[] distance = new int[count];
-        
-        for (int i = 0; i < count; i++) {
-            visitedVertex[i] = false;
-            distance[i] = Integer.MAX_VALUE;
-        }
+        HashSet<String> visitedSet =  new HashSet<>();
+        Map<String, Integer> distance = new HashMap<String, Integer>();
         
         // creating priority object to store nodes with their distance from source
-        PriorityQueue<Temp> objectQ = new PriorityQueue<>((Comparator.comparingInt(o -> o.distance)));
+        PriorityQueue<String> objectQ = new PriorityQueue<>((o1,o2)->distance.getOrDefault(o1,Integer.MAX_VALUE)-distance.getOrDefault(o2,Integer.MAX_VALUE));
     
         // Distance of self loop is zero
-        distance[source] = 0;
+        distance.put(source,0);
         
         //now adding source node to priority objectQ
-        objectQ.add(new Temp(source, distance[source]));
+        objectQ.add(source);
 
-        while (objectQ.size()>0){
+        while (!objectQ.isEmpty()){
 
-            Temp NewNode = objectQ.poll(); // to get out the minimum distance node from priority objectQ
+            String NewNode = objectQ.poll(); // to get out the minimum distance node from priority objectQ
 
-            if(visitedVertex[NewNode.vertex]) continue; // if NewNode is visited, do not add it or skip it
+            if(visitedSet.contains(NewNode)) continue; // if NewNode is visited, do not add it or skip it
 
-            visitedVertex[NewNode.vertex] = true; // if not visited mark as visited 
+            visitedSet.add(NewNode);// if not visited mark as visited 
         
             // creating new map that take edges from current nodes
 
-            Map<Integer, Integer> adjacentEdges = NewGraph.getAdjacent(NewNode.vertex); 
+            Map<String, Integer> adjacentEdges = NewGraph.getAdjacent(NewNode); 
     
             // now loop through the adjacent nodes 
-            for(int adjacentNode : adjacentEdges.keySet()){// adjacent nodes
+            for(String adjacentNode : adjacentEdges.keySet()){// adjacent nodes
 
-                if(visitedVertex[adjacentNode])continue; // if adjacent node visited, do not add or skip it
-            
+                if(visitedSet.contains(adjacentNode)) continue; // if adjacent node visited, do not add or skip it
                 // if new distance of adjacent new node is less than already noted distance than change id and add to objectQ
-                if(NewNode.distance + adjacentEdges.get(adjacentNode) < distance[adjacentNode]){ 
+                if(distance.get(NewNode) + adjacentEdges.get(adjacentNode) < distance.getOrDefault(adjacentNode, Integer.MAX_VALUE)){ 
+                    
                     // updat distance                                                                    
-                    distance[adjacentNode] = NewNode.distance + adjacentEdges.get(adjacentNode);
+                    distance.put(adjacentNode, distance.get(NewNode) + adjacentEdges.getOrDefault(adjacentNode, Integer.MAX_VALUE));
             
                     //  finally add the shortest distance to objectQ
-                    objectQ.add(new Temp(adjacentNode,distance[adjacentNode])); 
+                    objectQ.add(NewNode); 
                 }
             }
         }
-            System.out.println(Arrays.toString(distance));
+        // System.out.println(Arrays.toString(distance));
+        return distance;
     // a helpful website we used to create the dijkstra method https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-using-priority_queue-stl/
     }
  public static void main(String[] args) {
      
-        SymbolGraph<Integer> draw = new SymbolGraph<>();
+        SymbolGraph<String> draw = new SymbolGraph<>();
 
-        int NewGraph[][] = new int[][] { { 0, 0, 1, 2, 0, 0, 0 }, { 0, 0, 2, 0, 0, 3, 0 },
-                { 1, 2, 0, 1, 3, 0, 0 },{ 2, 0, 1, 0, 0, 0, 1 },
-                { 0, 0, 3, 0, 0, 2, 0 }, { 0, 3, 0, 0, 2, 0, 1 },
-                { 0, 0, 0, 1, 0, 1, 0 } };
+        draw.addVertex("A");
+        draw.addVertex("B");
+        draw.addVertex("C");
+        draw.addEdge("A","C",2);
+        draw.addEdge("A","B",2);
+        draw.addEdge("B","C",1);
+
+        // int NewGraph[][] = new int[][] { { 0, 0, 1, 2, 0, 0, 0 }, { 0, 0, 2, 0, 0, 3, 0 },
+        //         { 1, 2, 0, 1, 3, 0, 0 },{ 2, 0, 1, 0, 0, 0, 1 },
+        //         { 0, 0, 3, 0, 0, 2, 0 }, { 0, 3, 0, 0, 2, 0, 1 },
+        //         { 0, 0, 0, 1, 0, 1, 0 } };
                 
-        // use loop to add vertex to graph class
+        // // use loop to add vertex to graph class
 	 
-        for (int i=0;i < NewGraph.length;i++) draw.addVertex(i);
+        // for (int i=0;i < NewGraph.length;i++)draw.addVertex(i);
         
-        // use loop to add edges to edges class
+        // // use loop to add edges to edges class
 	 
-        for (int i=0; i< NewGraph.length;i++){
-            for(int j = 0;j < NewGraph.length;j++){
-                if(NewGraph[i][j]!=0){
+        // for (int i=0; i< NewGraph.length;i++){
+        //     for(int j = 0;j < NewGraph.length;j++){
+        //         if(NewGraph[i][j]!=0){
 			
-                    draw.addEdge(i,j,NewGraph[i][j]);
-                }
-            }
-        }
+        //             draw.addEdge(i,j,NewGraph[i][j]);
+        //         }
+        //     }
+        // }
+
         Dijkstra D= new Dijkstra();
 
-        D.dijkstra(draw, 0); //print short distance of all other nodes from node 0
+        Map<String, Integer> distance = D.dijkstra(draw, "C"); //print short distance of all other nodes from node 0
+
+        for (Map.Entry<String, Integer>  entry: distance.entrySet()) {
+            System.out.println("The shortest distance to "+entry.getKey()+" is " + entry.getValue());
+        }
 
         
     }
